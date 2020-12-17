f17c5ea2e45888820af1e96031f40e5943bc5bb5
96herman2 <76152822+96herman2@users.noreply.github.com>
2020-12-17 12:09:27 +0100

    Update edit-user.js
    Hi fellow student

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -6,8 +6,49 @@ class EditUser extends LitElement {
       user: { type: Object }
     };
   }
+  
+render(){
+    return html`
+    <head>
+    <title>Edit users: </title>
+    </head>
 
-  // din kode her
+    <body>
+    <form onsubmit="javascript: return false;" id="userFrom" method="POST">
+       <div>
+        <label for="email"> Email address: </label>
+        <input id="uname" name="uname">
+       </div>
 
+      <div>
+        <label for="firstName"> First Name: </label>
+        <input  id="firstName" name="firstName">
+      </div>
+
+      <div>
+        <label for="lastName">L ast Name: </label>
+        <input id="lastName" name="lastName">
+      </div>
+
+      <div>
+        <label for="oldpwd"> Old Password: </label>
+        <input type="password"  id="oldpwd" name="oldpwd">
+      </div>
+
+      <div>
+        <label for="newpwd"> New Password: </label>
+        <input type="password"  id="pwd" name="pwd">
+      </div>
+   </form>
+   </body>
+}
+
+  updateUser(e) {
+    const dataForm = new FormData(e.target.form);
+    fetch('api/updateUser.php'{
+     method: 'POST',
+     body: dataForm
+    }     
+  }  
 }
 customElements.define('edit-user', EditUser);
